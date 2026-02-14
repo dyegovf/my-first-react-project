d33841f62a602fcd5e4ff4386c2b40ff7010a517
Dyego <dyego.villar@run2biz.com>
2026-02-14 16:39:06 -0300

    concepts about useState with a simple counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  // let counter = 15
+
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    setCounter((prevCounter) => prevCounter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter((prevCounter) => prevCounter - 1)
+  }
+
+
+  return (
+    <>
+     <h1>React Course UseState{counter}</h1>
+     <h2>Counter value: {counter} </h2>
+     <button onClick={addValue}>Add value:</button>
+     <button onClick={removeValue}>Remove value</button>
+     <p>Footer: {counter} </p>
+    </>
+  )
+}
+
+export default App
