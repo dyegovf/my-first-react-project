df122b560625042ed1dfc6f6e9bf012a0db64696
Dyego <dyego.villar@run2biz.com>
2026-02-14 00:22:05 -0300

    Completion of introductory concepts of React App vs. React Vite

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,10 +1,11 @@
 import Youtube from './Youtube';
 
 function App() {
-  
-  return (
+  const username = "Dyego2"
+  return (    
     <>
-      <h1>vite react app</h1>
+      <h1>vite react app {2+2} </h1>
+      <h1>vite react app {username === "Dyego" ? "sim" : "não"} </h1>
       <Youtube />
     </>
     
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank',
+        style:'color: red; font-size: 20px;'
+    },
+    children: 'Click me now'
+}
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom React App Modified!!!</h1>            
+        </div>
+    )
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blank" style={{color: 'red', fontSize: '20px'}}>Visit Google 2222</a>
+)
+const username = "Dyego2"
+const reactElement2 = React.createElement (
+
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank',
+        style: {color: 'blue', fontSize: '20px'}
+    },
+    'Click here 2222',
+    username
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    reactElement2
   )
